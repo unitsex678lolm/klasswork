512f632bc86321a30c4780f1aa89f66e810dda69
unitsex678lolm <160113099+unitsex678lolm@users.noreply.github.com>
2024-02-15 12:05:58 +0300

    15.02.2024 пр 1 зад 3

---
+++ main (7).py
@@ -0,0 +1,9 @@
+a = input("ысе в поряде?")
+if a=="да" or a=="Да":
+    print("гамно")
+elif a=="неть" or a=="Неть":
+    print("")
+elif a=="да" or a=="Да":
+    print("все хорошо")
+else:
+    print("ужас")
